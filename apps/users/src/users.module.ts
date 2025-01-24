6fc03f8e737d0101e430a3f80bed62cb09d2337a
Virtue Egerega <egeregav@gmail.com>
2025-01-24 13:33:36 +0100

    Feature- added unique email check during create account

--- apps/users/src/users.module.ts
+++ apps/users/src/users.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseService } from '../database/database.service';
 
 @Module({
   imports: [
@@ -11,6 +12,6 @@ import { ConfigModule } from '@nestjs/config';
     })
   ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, DatabaseService],
 })
 export class UsersModule {}
--- apps/users/src/users.service.ts
+++ apps/users/src/users.service.ts
@@ -2,36 +2,56 @@
 import { ConflictException, Injectable, InternalServerErrorException, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
-import { Prisma } from '@prisma/client';
+import { $Enums, Prisma } from '@prisma/client';
+import { CreateUserDto, UpdateUserDto, UserDto } from '@shared/contracts';
+import { DatabaseService } from '../database/database.service';
 
-import { CreateUserDto, UpdateUserDto } from '@shared/contracts';
 @Injectable()
 export class UsersService {
-  // Promise<Prisma.UserCreateInput>
-  async create(createUserDto: CreateUserDto): Promise<CreateUserDto> {
+  constructor(private readonly databaseService: DatabaseService) { }
+  
+  
+  // Promise<Prisma.UserDto>
+  async create(createUserDto: CreateUserDto): Promise<UserDto> {
     const newUser: Prisma.UserCreateInput = {
-      firstname: createUserDto.firstName,
-      lastname: createUserDto.lastName,
+      firstName: createUserDto.firstName,
+      lastName: createUserDto.lastName,
       email: createUserDto.email,
       password: await bcrypt.hash(createUserDto.password, 10),
-      userType: createUserDto.userType,
-      status: 'ACTIVE'
+      userType: createUserDto.userType as $Enums.UserType,
+      status: 'ACTIVE' as $Enums.UserStatus
     }
 
-    // // create new user if email does not exist
-    // const createUser = await this.databaseService.user.create({ data: newUser })
+    //  check if the provided email is a registered email
+    const IsEmailNotUnique = await this.databaseService.user.findFirst({
+      where: {
+        email: newUser.email
+      }
+    });
 
-    // // //  if an error occurs during creation of new user, throw exceoption
-    // if (!createUser) {
+    // if email is not unique and contains a value, throw error cos user exists already
+    if (IsEmailNotUnique) {
+      console.log("We have an existing user with this email, kindly login to your account")
+      throw new ConflictException('This email has been used, kindly login to your account', {
+        cause: new Error(),
+        description: 'existing user'
+      });
 
-    //   console.log("could not create user")
-    //   throw new InternalServerErrorException('sever error could not create user', {
-    //     cause: new Error(),
-    //     description: 'user not found'
-    //   });
-    // }
-    // return createUser;
-    return createUserDto;
+    }
+    
+    // create new user if email does not exist
+    const createUser = await this.databaseService.user.create({ data: newUser })
+
+    //  if an error occurs during creation of new user, throw exeception
+    if (!createUser) {
+
+      console.log("could not create user")
+      throw new InternalServerErrorException('sever error could not create user', {
+        cause: new Error(),
+        description: 'could not create user'
+      });
+    }
+    return createUser;
   }
 
   findAll() {
