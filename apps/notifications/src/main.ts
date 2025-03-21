38f85945e54e8e51627ddbe6877b544c7a0dca90
Virtue Egerega <egeregav@gmail.com>
2025-03-21 18:50:17 +0100

    dummy app for notification listening on port 8006

--- apps/notifications/src/main.ts
+++ apps/notifications/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { NotificationsModule } from './notifications.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import * as dotenv from 'dotenv';
+import * as express from 'express';
+
 dotenv.config({ path: './apps/notifications/.env' });
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
@@ -19,5 +21,11 @@ async function bootstrap() {
   );
   await app.listen();
   console.log('Notification Microservice is listening...');
+
+  
+  // Dummy Express Server to satisfy Render
+  const dummyApp = express();
+  const port = process.env.PORT || 8001;
+  dummyApp.listen(port, () => console.log(`Dummy server running on port ${port}`));
 }
 bootstrap();
