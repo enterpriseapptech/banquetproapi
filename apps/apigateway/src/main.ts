0542f2a38aff314945515bef2636a5f757d2006b
Virtue Egerega <egeregav@gmail.com>
2025-09-18 09:23:43 +0100

    Update: cleaned up double implementation on apigateway

--- apps/apigateway/src/main.ts
+++ apps/apigateway/src/main.ts
@@ -4,41 +4,34 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*', // Allow all origins
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
-    allowedHeaders: 'Content-Type, Authorization', // Allowed headers
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-  })
-  
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // Remove unknown fields
-    forbidNonWhitelisted: true, // throws if unknown properties exist
-    forbidUnknownValues: true, // prevents non-object values in DTOs
-    transform: true, // Transform payload into DTO instance
-  }));
-app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,          // strips unknown properties
-      forbidNonWhitelisted: true, // throws if unknown properties exist
-      transform: true,          // transforms payloads to DTO classes
-      forbidUnknownValues: true // prevents non-object values in DTOs
-    }),
-  );
-  // Swagger Configuration
-  const config = new DocumentBuilder()
-    .setTitle('My Microservice API')
-    .setDescription('API documentation for my NestJS microservice')
-    .setVersion('1.0')
-    .addTag('microservice')
-    .build();
-  
-  
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+	const app = await NestFactory.create(AppModule);
+	app.enableCors({
+		origin: '*', // Allow all origins
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+		allowedHeaders: 'Content-Type, Authorization', // Allowed headers
+		credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+	})
 
-  await app.listen(8000);
+	app.useGlobalPipes(new ValidationPipe({
+		whitelist: true, // Remove unknown fields
+		forbidNonWhitelisted: true, // throws if unknown properties exist
+		forbidUnknownValues: true, // prevents non-object values in DTOs
+		transform: true, // Transform payload into DTO instance
+	}));
+	
+	// Swagger Configuration
+	const config = new DocumentBuilder()
+		.setTitle('My Microservice API')
+		.setDescription('API documentation for my NestJS microservice')
+		.setVersion('1.0')
+		.addTag('microservice')
+		.build();
+
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api/docs', app, document);
+
+	await app.listen(8000);
 }
 
 bootstrap();
